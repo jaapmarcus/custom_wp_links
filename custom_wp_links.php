87a0035053cf972a078a59b9ab0b35a400453c58
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-08-10 10:53:48 +0200

    Initial release

---
+++ custom_wp_links.php
@@ -0,0 +1,169 @@
+<?php
+/*
+Plugin Name: custom_wp_links
+Plugin URI: https://schipbreukeling.nl
+Description:
+Version: 1.0.1
+Author: Jaap Marcus
+Author URI: https://schipbreukeling.nl
+Text Domain: -
+*/
+
+if (!defined('ABSPATH')) {
+	exit();
+}
+
+class Custom_WP_links
+{
+	public const NAME = 'custom_wp_links';
+	public const VERSION = '1.0.1';
+
+	private static $instance = null;
+	public static $plugin_basename = null;
+	public static $is_configured = false;
+
+	public static function get_instance()
+	{
+		if (!self::$instance) {
+			self::$instance = new self();
+		}
+	
+		return self::$instance;
+	}
+	
+	function __construct(){
+			add_action('init', array($this, 'init'));			
+	}
+	
+	function init(){
+		add_filter('wp_link_pages', array($this,'my_page_nav'));	
+		add_action('wp_head',array($this, 'addHeader'));
+		$this::$is_configured = true;
+		
+		if (is_admin()) {
+			require_once __DIR__ . '/includes/admin.php';
+			$admin = new Custom_Wp_Links_Admin();
+		}
+
+	}
+	
+	public function addHeader(){
+			wp_enqueue_style('wp-pagenav', plugins_url("custom_wp_links/style.css"), array(),$this::VERSION, false);
+			$option = get_option('custom_wp_links');
+			echo "<style>";
+			if(!empty($option['nextcolor'])){
+				echo ".nextpostslink{ background-color: ".$option['nextcolor']." !important;}\n";
+			}
+			if(!empty($option['prevcolor'])){
+				echo ".previouspostslink{ background-color: ".$option['prevcolor']."  !important;;}\n";
+			}
+			if(!empty($option['textcolor'])){
+				echo ".previouspostslink, .nextpostslink{ color: ".$option['textcolor']." !important;}\n";
+			}
+
+			echo "</style>"; 
+	}
+	
+	function create_link($i){
+		global $wp_rewrite;
+		$post       = get_post();
+		$query_args = array();
+		
+		if (1 == $i) {
+				$url = get_permalink();
+		} else {
+				if (! get_option('permalink_structure') || in_array($post->post_status, array( 'draft', 'pending' ), true)) {
+						$url = add_query_arg('page', $i, get_permalink());
+				} elseif ('page' === get_option('show_on_front') && get_option('page_on_front') == $post->ID) {
+						$url = trailingslashit(get_permalink()) . user_trailingslashit("$wp_rewrite->pagination_base/" . $i, 'single_paged');
+				} else {
+						$url = trailingslashit(get_permalink()) . user_trailingslashit($i, 'single_paged');
+				}
+		}
+		
+		if (is_preview()) {
+				if (('draft' !== $post->post_status) && isset($_GET['preview_id'], $_GET['preview_nonce'])) {
+						$query_args['preview_id']    = wp_unslash($_GET['preview_id']);
+						$query_args['preview_nonce'] = wp_unslash($_GET['preview_nonce']);
+				}
+		
+				$url = get_preview_post_link($post, $query_args, $url);
+		}
+		return esc_url($url);
+	}
+	
+	function my_page_nav($args = '')
+	{
+			global $page, $page_number, $numpages, $multipage, $more;
+			$defaults = array(
+					'before'           => '<aside class="page-links pagination-wrap ">',
+					'after'            => '</aside>',
+					'link_before'      => '',
+					'link_after'       => '',
+					'aria_current'     => 'page',
+					'next_or_number'   => 'number',
+					'separator'        => ' ',
+					'nextpagelink'     => __('Next page'),
+					'previouspagelink' => __('Previous page'),
+					'pagelink'         => '%',
+					'echo'             => 1,
+			);
+			
+			$option = get_option('custom_wp_links');
+			$default = array('lastlink' => '/trending/', 'prevpage' => '<<', 'nextpagelong' => 'Volgende Pagina >>', 'nextpageshort' => '>>');
+			$option = array_merge($default, $option);
+			$parsed_args = wp_parse_args($args, $defaults);
+			$my_current_lang = apply_filters( 'wpml_current_language', NULL );
+			if (get_post_type() != 'page') {
+					?>
+			<div class="wp-page-nav" role="navigation">
+					<?php if ($page > 1) {
+							?>
+					<a class="previouspostslink" rel="prev" aria-label="Previous Page" href="<?php echo $this ->create_link($page - 1); ?>"><?=$option['prevpage'];?></a>
+					<?php
+					}
+					if ($page < $numpages) {
+							?>
+					<a class="nextpostslink large-link" rel="next" aria-label="Next Page" href="<?php echo $this ->create_link ($page +1); ?>"><?=$option['nextpagelong'];?></a>
+					<a class="nextpostslink small-link"  rel="next" aria-label="Next Page" href="<?php echo $this ->create_link($page +1); ?>"><?=$option['nextpageshort'];?></a>
+					<?php
+					}else{
+		?>
+					<a class="nextpostslink large-link" rel="next" aria-label="Next Page" href="<?=$option['lastlink'];?>"><?=$option['nextpagelong'];?></a>
+					<a class="nextpostslink small-link"  rel="next" aria-label="Next Page" href="<?=$option['lastlink'];?>"><?=$option['nextpageshort'];?></a>
+					<?php
+		}
+		 ?>
+			</div>
+	<?php
+	}
+	}
+	
+	
+}
+
+Custom_WP_links::get_instance();
+
+/* Add Next Page Button in First Row */
+add_filter( 'mce_buttons', 'my_add_next_page_button', 1, 2 ); // 1st row
+ 
+/**
+ * Add Next Page/Page Break Button
+ * in WordPress Visual Editor
+ * 
+ * @link https://shellcreeper.com/?p=889
+ */
+function my_add_next_page_button( $buttons, $id ){
+ 
+		/* only add this for content editor */
+		if ( 'content' != $id )
+				return $buttons;
+ 
+		/* add next page after more tag button */
+		array_splice( $buttons, 13, 0, 'wp_page' );
+ 
+		return $buttons;
+}
+
+
+?>
---
+++ includes/admin.php
@@ -0,0 +1,136 @@
+<?php 
+if (!defined('ABSPATH')) {
+	exit();
+}
+
+class Custom_Wp_Links_Admin{
+		private $plugin = null;
+		
+		public function __construct()
+		{
+			$this->plugin = Custom_WP_links::get_instance();
+		
+			$options = get_option($this->plugin::NAME);
+		
+		
+			// Add settings link to plugin actions.
+			add_filter('plugin_action_links_' . $this->plugin::$plugin_basename, array($this, 'settings_link'));
+			
+			// Add settings page.
+			add_action('admin_init', array($this, 'register_settings'));
+			add_action('admin_menu', array($this, 'add_settings_page'));
+			
+			
+		}
+		
+		function settings_link($links)
+		{
+			$url = esc_url(add_query_arg(
+				'page',
+				$this->plugin::NAME,
+				get_admin_url() . 'options-general.php'
+			));
+		
+			array_push($links, "<a href='$url'>" . __('Settings', $this->plugin::NAME) . '</a>');
+			return $links;
+		}
+		
+		public function register_settings()
+		{
+			register_setting(
+				$this->plugin::NAME,
+				$this->plugin::NAME,
+				array('sanitize_callback' => array($this, 'validate_options'))
+			);
+		
+			add_settings_section('custom_wp_link_settings', 'Custom WP Link Settings', '', $this->plugin::NAME);
+			add_settings_field('custom_wp_link_last_link', 'Laatste link', array($this, 'custom_wp_link_last_link'), $this->plugin::NAME, 'custom_wp_link_settings');
+			add_settings_field('custom_wp_link_text_next_page', 'Volgende pagina (Web)', array($this, 'custom_wp_link_text_next_page'), $this->plugin::NAME, 'custom_wp_link_settings');
+			add_settings_field('custom_wp_link_text_next_page_short', 'Volgende pagina (Mob)', array($this, 'custom_wp_link_text_next_page_short'), $this->plugin::NAME, 'custom_wp_link_settings');
+			add_settings_field('custom_wp_link_text_prev_page', 'Vorige pagina', array($this, 'custom_wp_link_text_prev_page'), $this->plugin::NAME, 'custom_wp_link_settings');
+			
+			add_settings_field('custom_wp_link_next_page_color', 'Volgende pagina Achterground Kleur', array($this, 'custom_wp_link_next_page_color'), $this->plugin::NAME, 'custom_wp_link_settings');
+						
+			add_settings_field('custom_wp_link_prev_page_color', 'Vorige pagina Achterground Kleur', array($this, 'custom_wp_link_prev_page_color'), $this->plugin::NAME, 'custom_wp_link_settings');
+			
+			add_settings_field('custom_wp_link_text_color', 'Vorige pagina Achterground Kleur', array($this, 'custom_wp_link_text_color'), $this->plugin::NAME, 'custom_wp_link_settings');
+		}
+		
+		public function custom_wp_link_last_link(){
+			$options = get_option($this->plugin::NAME);
+			$lastlink = $this->plugin::$is_configured ? esc_attr($options["lastlink"]) : "";
+			echo '<input id="custom_wp_link_last_link" name="' . $this->plugin::NAME . '[lastlink]" type="text" value="' . $lastlink . '" />';
+		}
+		
+		public function custom_wp_link_text_next_page(){
+			$options = get_option($this->plugin::NAME);
+			$nextpagelong = $this->plugin::$is_configured ? esc_attr($options["nextpagelong"]) : "";
+			echo '<input id="custom_wp_link_text_next_page" name="' . $this->plugin::NAME . '[nextpagelong]" type="text" value="' . $nextpagelong . '" />';
+		}
+		
+		public function custom_wp_link_text_next_page_short(){
+			$options = get_option($this->plugin::NAME);
+			$nextpageshort = $this->plugin::$is_configured ? esc_attr($options["nextpageshort"]) : "";
+			echo '<input id="custom_wp_link_text_next_page" name="' . $this->plugin::NAME . '[nextpageshort]" type="text" value="' . $nextpageshort . '" />';
+		}
+		public function custom_wp_link_text_prev_page(){
+			$options = get_option($this->plugin::NAME);
+			$prevpage = $this->plugin::$is_configured ? esc_attr($options["prevpage"]) : "";
+			echo '<input id="custom_wp_link_text_next_page" name="' . $this->plugin::NAME . '[prevpage]" type="text" value="' . $prevpage . '" />';
+		}
+		
+		public function custom_wp_link_next_page_color(){
+			$options = get_option($this->plugin::NAME);
+			$nextcolor = $this->plugin::$is_configured ? esc_attr($options["nextcolor"]) : "";
+			echo '<input id="custom_wp_link_text_next_page" name="' . $this->plugin::NAME . '[nextcolor]" type="text" value="' . $nextcolor . '" />';
+		}
+		
+		public function custom_wp_link_prev_page_color(){
+			$options = get_option($this->plugin::NAME);
+			$prevcolor = $this->plugin::$is_configured ? esc_attr($options["prevcolor"]) : "";
+			echo '<input id="custom_wp_link_text_next_page" name="' . $this->plugin::NAME . '[prevcolor]" type="text" value="' . $prevcolor . '" />';
+		}
+		
+		public function custom_wp_link_text_color(){
+			$options = get_option($this->plugin::NAME);
+			$textcolor = $this->plugin::$is_configured ? esc_attr($options["textcolor"]) : "";
+			echo '<input id="custom_wp_link_text_next_page" name="' . $this->plugin::NAME . '[textcolor]" type="text" value="' . $textcolor . '" />';
+		}
+
+		
+		public function add_settings_page()
+		{
+			add_options_page(
+				__('Custom WP Links', $this->plugin::NAME),
+				__('Custom WP Links', $this->plugin::NAME),
+				'manage_options',
+				$this->plugin::NAME,
+				array($this, 'render_settings_page')
+			);
+		}
+		
+		public function validate_options($input)
+		{
+			$options = get_option($this->plugin::NAME);	
+				
+			return $input;
+		}
+		
+		public function render_settings_page()
+			{
+		?>
+				<div class="wrap">
+					<h1><?php esc_html_e('Custom WP Links', $this->plugin::NAME); ?></h1>
+		
+					<form method="post" action="options.php">
+						<?php
+						settings_fields($this->plugin::NAME);
+						do_settings_sections($this->plugin::NAME);
+						submit_button();
+						?>
+					</form>
+				</div>
+		<?php
+		}
+		
+}
