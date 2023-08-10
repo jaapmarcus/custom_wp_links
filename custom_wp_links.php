add8f03b0b0d43ca56fcf22e1c36fbcca8462610
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-08-10 10:56:05 +0200

    Remove page break

--- custom_wp_links.php
+++ custom_wp_links.php
@@ -144,26 +144,5 @@ class Custom_WP_links
 
 Custom_WP_links::get_instance();
 
-/* Add Next Page Button in First Row */
-add_filter( 'mce_buttons', 'my_add_next_page_button', 1, 2 ); // 1st row
- 
-/**
- * Add Next Page/Page Break Button
- * in WordPress Visual Editor
- * 
- * @link https://shellcreeper.com/?p=889
- */
-function my_add_next_page_button( $buttons, $id ){
- 
-		/* only add this for content editor */
-		if ( 'content' != $id )
-				return $buttons;
- 
-		/* add next page after more tag button */
-		array_splice( $buttons, 13, 0, 'wp_page' );
- 
-		return $buttons;
-}
-
 
 ?>
